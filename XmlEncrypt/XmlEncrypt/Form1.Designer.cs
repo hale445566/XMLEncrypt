8c98fb04ac8888f674d00997306e106d70717d35
hale445566 <848130243@qq.com>
2016-02-25 17:19:21 +0800

    添加文件

---
+++ XmlEncrypt/XmlEncrypt/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace XmlEncrypt
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.LoadXMl = new System.Windows.Forms.Button();
+            this.KeyBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SaveButton = new System.Windows.Forms.Button();
+            this.XmlTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // LoadXMl
+            // 
+            this.LoadXMl.Location = new System.Drawing.Point(533, 582);
+            this.LoadXMl.Name = "LoadXMl";
+            this.LoadXMl.Size = new System.Drawing.Size(75, 23);
+            this.LoadXMl.TabIndex = 0;
+            this.LoadXMl.Text = "Load";
+            this.LoadXMl.UseVisualStyleBackColor = true;
+            this.LoadXMl.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // KeyBox
+            // 
+            this.KeyBox.Location = new System.Drawing.Point(112, 582);
+            this.KeyBox.Name = "KeyBox";
+            this.KeyBox.Size = new System.Drawing.Size(367, 21);
+            this.KeyBox.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(77, 587);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 12);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "密钥";
+            // 
+            // SaveButton
+            // 
+            this.SaveButton.Location = new System.Drawing.Point(631, 582);
+            this.SaveButton.Name = "SaveButton";
+            this.SaveButton.Size = new System.Drawing.Size(75, 23);
+            this.SaveButton.TabIndex = 3;
+            this.SaveButton.Text = "Save";
+            this.SaveButton.UseVisualStyleBackColor = true;
+            this.SaveButton.Click += new System.EventHandler(this.SaveButton_Click);
+            // 
+            // XmlTextBox
+            // 
+            this.XmlTextBox.Location = new System.Drawing.Point(12, 12);
+            this.XmlTextBox.Multiline = true;
+            this.XmlTextBox.Name = "XmlTextBox";
+            this.XmlTextBox.Size = new System.Drawing.Size(694, 564);
+            this.XmlTextBox.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(727, 617);
+            this.Controls.Add(this.XmlTextBox);
+            this.Controls.Add(this.SaveButton);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.KeyBox);
+            this.Controls.Add(this.LoadXMl);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button LoadXMl;
+        private System.Windows.Forms.TextBox KeyBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button SaveButton;
+        private System.Windows.Forms.TextBox XmlTextBox;
+    }
+}
+
---
+++ XmlEncrypt/XmlEncrypt/Form1.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace XmlEncrypt
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //Load Button
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog file = new OpenFileDialog();
+            file.Filter = "XML文件|*.xml";
+            file.RestoreDirectory = true;
+
+            if (file.ShowDialog() == DialogResult.OK)
+            {
+                LoadXML(file.FileName.ToString());
+            }
+        }
+
+        // Save Button
+        private void SaveButton_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog file = new OpenFileDialog();
+            file.Filter = "XML文件|*.xml";
+            file.RestoreDirectory = true;
+
+            if (file.ShowDialog() == DialogResult.OK)
+            {
+                SaveXML(file.FileName.ToString());
+            }
+        }
+
+        private void LoadXML(string path)
+        {
+            XmlTextBox.Clear();
+            XmlTextBox.Text = DecrtyptLoadXML(path);
+        }
+
+        private void SaveXML(string path) {
+            EncrtyptSaveXML(path);
+        }
+
+        //保存加密文件
+        private void EncrtyptSaveXML(string xmlpath)
+        {
+            string xmlData = XmlTextBox.Text;
+            string xxx = Encrypt(xmlData);
+            StreamWriter writer;
+            writer = File.CreateText(xmlpath);
+            writer.Write(xxx);
+            writer.Close();
+        }
+
+        //读取加密文件
+        public string DecrtyptLoadXML(string xmlpath) {
+            StreamReader sReader = File.OpenText(xmlpath);
+            string xmlData = sReader.ReadToEnd();
+            sReader.Close();
+            string xxx = Decrypt(xmlData);
+            return xxx;
+        }
+
+        //加密
+        private string Encrypt(string toE)
+        {
+            if (KeyBox.Text != "")
+            {
+                byte[] keyArray = UTF8Encoding.UTF8.GetBytes(KeyBox.Text);
+                RijndaelManaged rDel = new RijndaelManaged();
+                rDel.Key = keyArray;
+                rDel.Mode = CipherMode.ECB;
+                rDel.Padding = PaddingMode.PKCS7;
+                ICryptoTransform cTransform = rDel.CreateEncryptor();
+                byte[] toEncryptArray = UTF8Encoding.UTF8.GetBytes(toE);
+                byte[] resultArray = cTransform.TransformFinalBlock(toEncryptArray, 0, toEncryptArray.Length);
+
+                return Convert.ToBase64String(resultArray, 0, resultArray.Length);
+            }
+            else
+                return toE;
+        }
+
+        //解密
+        private string Decrypt(string toD)
+        {
+            if (KeyBox.Text != "")
+            {
+                //加密和解密采用相同的key,具体值自己填，但是必须为32位
+                byte[] keyArray = UTF8Encoding.UTF8.GetBytes(KeyBox.Text);
+                RijndaelManaged rDel = new RijndaelManaged();
+                try
+                {
+                    rDel.Key = keyArray;
+                }
+                catch
+                {
+                    return "密钥长度错误";
+                }
+                rDel.Mode = CipherMode.ECB;
+                rDel.Padding = PaddingMode.PKCS7;
+                ICryptoTransform cTransform = rDel.CreateDecryptor();
+                byte[] toEncryptArray;
+                byte[] resultArray;
+                try
+                {
+                    toEncryptArray = Convert.FromBase64String(toD);
+                    resultArray = cTransform.TransformFinalBlock(toEncryptArray, 0, toEncryptArray.Length);
+
+                }
+                catch
+                {
+                    return "密钥错误";
+                }
+
+                return UTF8Encoding.UTF8.GetString(resultArray);
+            }
+            else
+                return toD;
+        }
+
+        
+    }
+}
